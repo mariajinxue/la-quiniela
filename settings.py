ee685cbe2e4defb3e82cd1a6338a03d78ad73ca5
mariajinxue <149168864+mariajinxue@users.noreply.github.com>
2023-11-17 13:38:52 +0100

    Modelo en python
    Subo los modelos de python hechos, como mucho hacer algún retoque en nombres etc. pero ya funcionan, olee!!!!

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 
 DATABASE_PATH = BASE_DIR / "laliga.sqlite"
+DATABASE_PATH_1 = BASE_DIR / "clasification.sqlite"
 
 MODELS_PATH = BASE_DIR / "models"
 
